3291b0fc4c6a05e38f8aa1a98617e14b418a8eca
Jeremie Tamburini <jeremie.tamburini@gmail.com>
2026-09-02 18:46:27 +0200

    Forgot to add history.rs :P

---
+++ src/history.rs
@@ -0,0 +1,42 @@
+use crate::calculation::Calculation;
+
+pub struct History {
+    records: Vec<Calculation, f64>
+}
+
+impl History {
+    pub fn new() -> Self {
+        Self { records: Vec::new() }
+    }
+
+    pub fn add(&mut self, calc: Calculation, result: f64) {
+        self.records.push((calc, result))
+    }
+
+    pub fn display(&self) {
+        println!("\n📜 ::: OPERATION HISTORY :::");
+        if self.records.is_empty() {
+            println!("History is empty.");
+        } else {
+            for (index, (calc, res)) in self.records.iter().enumerate() {
+                println!("[{}] {} = {}", index + 1, calc, res);
+            }                        
+        }
+        println!("---------------------------------------\n");
+    }
+    
+    pub fn is_empty(&self) -> bool {
+        self.records.is_empty()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
